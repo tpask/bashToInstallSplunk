831e3a786b70d90d116543eeb05169783f3bb604
Thien <hairyk9@gmail.com>
2018-09-23 23:56:21 -0700

    added aws cli

--- installSplunk.sh
+++ installSplunk.sh
@@ -9,14 +9,6 @@ if [ ! -z "$host" ]; then
   hostnamectl set-hostname $host
 fi
 
-# install aws cli if region exists
-if [ ! -z "$region" ]; then
-  curl -O https://bootstrap.pypa.io/get-pip.py
-  python get-pip.py 
-  pip install awscli 
-  aws sns publish --topic-arn $topic --region $region  --message "$HOSTNAME is up"
-fi
-
 #yum update
 yum update -y
 
@@ -26,16 +18,25 @@ hash='$6$8Col5Rup5yZngDMQ$NkanFwDqNPUkUiqLUGR17DekyTkBsJQEBPfva6oXv61v6gEUz2azZI
 splunk_url='https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.3&product=splunk&filename=splunk-7.1.3-51d9cac7b837-linux-2.6-x86_64.rpm&wget=true'
 splunk_rpm=`echo $splunk_url | awk -F'[=&]' '{print $10}'`
 SPLUNK_HOME=/opt/splunk
-cd
+cd /tmp
 yum install wget -y
 wget -O $splunk_rpm $splunk_url
 rpm -ivh $splunk_rpm
 
-cat > $SPLUNK_HOME/etc/system/local/user-seed.conf << EOF 
-[user_info]
-USERNAME = admin
-HASHED_PASSWORD = $hash
-EOF
-$SPLUNK_HOME/bin/splunk start --answer-yes --no-prompt --accept-license
+#if splunk-passwd is set use it, else initial password is "changeme"
+if [ -z "$splunk_passwd" ]; then 
+  splunk_passwd="changeme"
+fi 
+$SPLUNK_HOME/bin/splunk start --answer-yes --no-prompt --accept-license --seed-passwd $splunk_passwd
 $SPLUNK_HOME/bin/splunk enable boot-start 
 
+
+#this section is only for AWS instnaces
+# install aws cli if region exists
+if [ ! -z "$region" ]; then
+  curl -O https://bootstrap.pypa.io/get-pip.py
+  python get-pip.py
+  pip install awscli
+  aws sns publish --topic-arn $topic --region $region --message "$HOSTNAME is up"
+  aws ec2 create-tags --resources $instanceId --tags Key=NAME,Value=$HOSTNAME
+fi
