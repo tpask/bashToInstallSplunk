2bfd6f4dbda545ac1e10cf3c21eb135128d57688
Thien <11982736+tpask@users.noreply.github.com>
2021-02-27 01:59:15 -0800

    Update installSplunk.sh

--- installSplunk.sh
+++ installSplunk.sh
@@ -2,7 +2,6 @@
 
 host=
 region=
-
 #set hostname if host var is set
 if [ ! -z "$host" ]; then
   hostnamectl set-hostname $host
@@ -14,7 +13,8 @@ yum update -y
 #install splunk
 #default admin password is "helloWorld" remember to change it.
 hash='$6$8Col5Rup5yZngDMQ$NkanFwDqNPUkUiqLUGR17DekyTkBsJQEBPfva6oXv61v6gEUz2azZIP0fOV0.HVYXiz6ZKvx9s3o8feqbUGZi.'
-splunk_url='https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.3&product=splunk&filename=splunk-7.1.3-51d9cac7b837-linux-2.6-x86_64.rpm&wget=true'
+splunk_url='https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.2&product=splunk&filename=splunk-8.1.2-545206cc9f70-linux-2.6-x86_64.rpm&wget=true'
+#splunk_url='https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.1.3&product=splunk&filename=splunk-7.1.3-51d9cac7b837-linux-2.6-x86_64.rpm&wget=true'
 splunk_rpm=`echo $splunk_url | awk -F'[=&]' '{print $10}'`
 SPLUNK_HOME=/opt/splunk
 cd /tmp
